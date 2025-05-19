e2108fea260a50c83500d08c9cc6162014e6f17b
Adam-S988 <adam.stevenson@keyin.com>
2025-05-19 13:59:54 -0230

    Got a lot of the markers working!

--- src/Pages/Map.jsx
+++ src/Pages/Map.jsx
@@ -1,15 +1,14 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
-import L from 'leaflet';
+import MapMarkers from "./MapMarker";
 import "./LanguageMap/Map.css";
 import "leaflet/dist/leaflet.css";
 
-// Utility component to fix map resizing issues
+// Utility to fix map sizing issues
 function MapFixer() {
   const map = useMap();
 
   useEffect(() => {
-    // Wait for the map container to fully mount
     setTimeout(() => {
       map.invalidateSize();
     }, 100);
@@ -19,6 +18,8 @@ function MapFixer() {
 }
 
 const LanguageMap = () => {
+  const [selectedYear, setSelectedYear] = useState("2500BC");
+
   const languageFamilies = [
     "Celtic",
     "Germanic",
@@ -31,6 +32,32 @@ const LanguageMap = () => {
     "Uralic",
     "PaleoEuropean",
   ];
+
+  const handleSliderChange = (e) => {
+const yearMap = {
+  1: "2500BC",
+  2: "2000BC",
+  3: "1500BC",
+  4: "1000BC",
+  5: "800BC",
+  6: "600BC",
+  7: "400BC",
+  8: "200BC",
+  9: "1AD",
+  10: "100AD",
+  11: "200AD",
+  12: "300AD",
+  13: "400AD",
+  14: "500AD",
+  15: "600AD",
+  16: "700AD",
+  17: "800AD",
+};
+
+    const newYear = yearMap[e.target.value];
+    setSelectedYear(newYear);
+    document.getElementById("yearLabel").innerText = newYear;
+  };
 
   return (
     <>
@@ -59,21 +86,23 @@ const LanguageMap = () => {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution="&copy; OpenStreetMap contributors"
             />
+            <MapMarkers year={selectedYear} />
           </MapContainer>
         </div>
       </div>
 
       <div id="controls">
         <label htmlFor="timeSlider">
-          Year: <span id="yearLabel">2500 BC</span>
+          Year: <span id="yearLabel">2500BC</span>
         </label>
         <input
           type="range"
           id="timeSlider"
           min="1"
-          max="5"
+          max="15"
           defaultValue="1"
           step="1"
+          onChange={handleSliderChange}
         />
         <button id="playButton">Play</button>
       </div>
