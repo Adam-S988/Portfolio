eb992e906d38e57adb45bdc2bfd9976a7e04b441
Adam-S988 <adam.stevenson@keyin.com>
2025-05-19 18:58:18 -0230

    Merged the Main and the About/Added Resume

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,14 +7,14 @@ function About() {
       <h1 class="aboutTitle">Hello, I'm Adam!</h1>
       <p className="about-text">
         <img src={AdamPic} id="aboutPic" alt="Adam" />
-        I'm a software development student at Keyin College in St. John's,
+        I'm a recent graduate from software development at Keyin College in St. John's,
         Newfoundland. This site showcases some of the projects I've built so
-        far. Some of these programs are from school projects while others are
-        based on various tutorials, with unique twists to make them my own. I
-        have learned a lot about web design and aspire to continue learning and
-        create modern, easy-to-use websites. Outside of school, I aspire to
+        far. Some of these programs are school projects while others are
+        based on various tutorials I've followed, with unique twists to make them my own.<br/>I
+        have learned a lot about web design and have continued learning with the goal to 
+        create modern, easy-to-use websites. I also have experience with SQL and NoSQL databases.<br/>Outside of school, I aspire to
         learn more about how to incorporate my interests of video games and
-        languages into practice.
+        languages into my projects.
       </p>
     </div>
   );
--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import AdamPic from "../Images/Adam.jpg";
 
 function Main() {
   console.log("Main component rendering");
   return (
     <div className="App">
-      <h1>Adam Stevenson.</h1>
+      <h1>Hello, I'm Adam!</h1>
       <h2>A coding portfolio</h2>
-      <p>
-        I am currently a student learning about software development. Here is
-        where I will be showcasing various projects I have been working on
-        during my studies.
+    <div className="AboutPage">
+      <p className="about-text">
+        <img src={AdamPic} id="aboutPic" alt="Adam" />
+        I'm a recent graduate from software development at Keyin College in St. John's,
+        Newfoundland. This site showcases some of the projects I've built so
+        far. Some of these programs are school projects while others are
+        based on various tutorials I've followed, with unique twists to make them my own.<br/>I
+        have learned a lot about web design and have continued learning with the goal to 
+        create modern, easy-to-use websites. I also have experience with SQL and NoSQL databases.<br/>Outside of school, I aspire to
+        learn more about how to incorporate my interests of video games and
+        languages into my projects.
       </p>
+    </div>
     </div>
   );
 }
